8971c141cd2ec359216f3c9536630c43169d80bd
root <root>
2018-05-27 11:35:31 +0800

    页面映射结束

--- src/main/java/com/mmall/controller/portal/IndexController.java
+++ src/main/java/com/mmall/controller/portal/IndexController.java
@@ -3,6 +3,7 @@ package com.mmall.controller.portal;
 import com.mmall.pojo.Check;
 import com.mmall.service.ICheckerService;
 import com.mmall.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,36 +12,99 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 @Controller
-@RequestMapping("/html")
+//@RequestMapping("/html")
 public class IndexController {
 
-    @Autowired
-    private ICheckerService iCheckerService;
-    @Autowired
-    private IUserService iUserService;
 
-    @RequestMapping("/welcome.do")
+    @RequestMapping("/jdw.do")
     public ModelAndView welcome(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        modelAndView.setViewName("/html/index");
         return modelAndView;
     }
 
-/*    @RequestMapping("/login.do")
-    public ModelAndView login(HttpSession session) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("html/login");
-
-        return modelAndView;
-    }*/
-    @RequestMapping("/testcheck.do")
-    @ResponseBody
-    public ModelAndView check(HttpSession session, Check check) {
-        System.out.println(check.getStationId());
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
-        return modelAndView;
+    //1.选择检测员
+    @RequestMapping("/selectperson.do")
+    public String selectPerson() {
+        return "/html/selectperson";
     }
+
+    @RequestMapping("paramset.do")
+    public String paramset() {
+        return "/html/paramset";
+    }
+
+    @RequestMapping("/selectstation.do")
+    public String selectStation() {
+        return "/html/selectstation";
+    }
+
+    @RequestMapping("selectjiediwang.do")
+    public String selectJiediwang() {
+        return "/html/selectjiediwang";
+    }
+
+    @RequestMapping("stationmanage.do")
+    public String stationmanage() {
+        return "/html/stationmanage";
+    }
+
+    @RequestMapping("addstation.do")
+    public String addstation() {
+        return "/html/addstation";
+    }
+
+    @RequestMapping("editstation.do")
+    public String editstation() {
+        return "/html/editstation";
+    }
+
+    @RequestMapping("jiediwangmanage.do")
+    public String jiediwangManage() {
+        return "/html/jiediwangmanage";
+    }
+
+    @RequestMapping("peoplemanage.do")
+    public String peoplemanage() {
+        return "/html/peoplemanage";
+    }
+
+    @RequestMapping("addperson.do")
+    public String addperson() {
+        return "/html/addperson";
+    }
+
+    @RequestMapping("addjiediwang.do")
+    public String addjiediwang() {
+        return "/html/addjiediwang";
+    }
+
+    @RequestMapping("editperson.do")
+    public String editperson() {
+        return "/html/editperson";
+    }
+
+    @RequestMapping("historyinfor.do")
+    public String historyinfor() {
+        return "/html/historyinfor";
+    }
+
+    @RequestMapping("managersetting.do")
+    public String managersetting() {
+        return "/html/managersetting";
+    }
+
+    @RequestMapping("revisepwd.do")
+    public String revisepwd() {
+        return "/html/revisepwd";
+    }
+
+    @RequestMapping("editjiediwang.do")
+    public String editjiediwang() {
+        return "/html/editjiediwang";
+    }
+
 
 }
