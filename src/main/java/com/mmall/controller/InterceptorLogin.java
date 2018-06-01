93e9db0dd04e2ec8ad90c3c481cd010910d8a27b
jin <jin.com>
2018-06-01 11:43:00 +0800

    修复搜索bug，加跳转链接

--- src/main/java/com/mmall/controller/InterceptorLogin.java
+++ src/main/java/com/mmall/controller/InterceptorLogin.java
@@ -3,6 +3,7 @@ package com.mmall.controller;
 import com.mmall.common.Const;
 import com.mmall.pojo.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,9 @@ public class InterceptorLogin extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("-----拦截请求-----");
+        log.info(request.getPathInfo());
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        log.info(handlerMethod.getMethod().getName());
         HttpSession session = request.getSession();
         //return true;
         User find = (User) session.getAttribute(Const.CURRENT_USER);
@@ -24,7 +28,7 @@ public class InterceptorLogin extends HandlerInterceptorAdapter {
             //PrintWriter printWriter = httpServletResponse.getWriter();
             //printWriter.write("{code:0,message:\"session is invalid,please login again!\"}");
             //跳转登录
-            String url = "/html/login.html";
+            String url = "http://localhost:8080/JDW/html/login.html";
             response.sendRedirect(url);
             return false;
         }
